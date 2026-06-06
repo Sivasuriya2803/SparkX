a08b1e12c0931667fefd1903230f14536f3c9d43
Sivasuriya <sivasuriyaitskct@gmail.com>
2026-06-06 19:00:42 +0530

    Initaln RevCast implementation

---
+++ RevCast/src/main/java/com/virtusa/RevCast/RevCast/RevCastApplication.java
@@ -0,0 +1,13 @@
+package com.virtusa.RevCast.RevCast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevCastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevCastApplication.class, args);
+	}
+
+}
---
+++ RevCast/src/test/java/com/virtusa/RevCast/RevCast/RevCastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virtusa.RevCast.RevCast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevCastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
